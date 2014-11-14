1355ade3519abf5a9b35237366094f83ed08c8e5
THOMIAS Yannis <thomias.yannis@gmail.com>
2014-11-14 23:12:01 +0100

    first

---
+++ JSFManagedBean/src/com/yt/sandbox/mbean/Message.java
@@ -0,0 +1,19 @@
+package com.yt.sandbox.mbean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "message", eager = true)
+@RequestScoped
+public class Message {
+
+	private String message = "Hello World!";
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
